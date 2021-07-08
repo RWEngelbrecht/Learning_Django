0cb9a7cc51fc1d8190e6b2d182f20823af27d22d
RWEngelbrecht <rigardt@brechtburg.co.za>
2021-07-08 09:10:30 +0200

    added and updated queryset getters for views

--- getting_started/mysite/polls/views.py
+++ getting_started/mysite/polls/views.py
@@ -1,38 +1,46 @@
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.db.models import F, QuerySet
+from django.db.models import F
 from django.views import generic
+from django.utils import timezone as tz
 
 from .models import Question, Choice
 
 class IndexView(generic.ListView):
-  # ListView generic view uses template called 
+  # ListView generic view uses template called
   # <appName>/<modelName>_detail.html unless specified
-  # otherwise with template_name attribute 
+  # otherwise with template_name attribute
   template_name = 'polls/index.html'
   context_object_name = 'latest_question_list'
 
   def get_queryset(self):
-    return Question.objects.order_by('-pub_date')[:5]
+    # returns QuerySet object
+    return Question.objects.filter(pub_date__lte=tz.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
   model = Question
-  # DetailView generic view uses template called 
+  # DetailView generic view uses template called
   # <appName>/<modelName>_detail.html unless specified
-  # otherwise with template_name attribute 
+  # otherwise with template_name attribute
   template_name = 'polls/detail.html'
+
+  def get_queryset(self):
+      return Question.objects.filter(pub_date__lte=tz.now())
 
 
 class ResultsView(generic.DetailView):
   model = Question
   template_name = 'polls/results.html'
 
+  def get_queryset(self):
+      return Question.objects.filter(pub_date__lte=tz.now())
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    
+
     try:
       selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
