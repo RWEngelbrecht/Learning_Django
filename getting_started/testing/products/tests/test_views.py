4598aa1e8369e67d7a311a3b05ff6118d00034c9
RWEngelbrecht <rigardt@brechtburg.co.za>
2021-07-14 08:18:25 +0200

    refactored to use fixtures

--- getting_started/testing/products/tests/test_views.py
+++ getting_started/testing/products/tests/test_views.py
@@ -7,28 +7,31 @@ from django.urls import reverse
 
 from products.views import product_detail
 
-@pytest.mark.django_db
-class TestViews(TestCase):
+@pytest.fixture
+def factory():
+  return RequestFactory()
 
-  # setUpClass will execute before any other tests run
-  @classmethod
-  def setUpClass(cls):
-    super(TestViews, cls).setUpClass()
-    mixer.blend('products.Product')
-    cls.factory = RequestFactory()
+@pytest.fixture(scope='module')
+def product(db):
+  print('FACTORY INSTANTIATED')
+  return mixer.blend('products.Product')
 
-  def test_product_detail_authenticated(self):
-    path = reverse('detail', kwargs={'pk': 1})
-    request = self.factory.get(path)
-    request.user = mixer.blend(User)
+def test_product_detail_authenticated(factory, product):
+  mixer.blend('products.Product')
+  path = reverse('detail', kwargs={'pk': 1})
+  request = factory.get(path)
+  request.user = mixer.blend(User)
 
-    response = product_detail(request, pk=1)
-    assert response.status_code == 200
+  response = product_detail(request, pk=1)
+  assert response.status_code == 200
 
-  def test_product_detail_unauthenticated(self):
-    path = reverse('detail', kwargs={'pk': 1})
-    request = self.factory.get(path)
-    request.user = AnonymousUser()
 
-    response = product_detail(request, pk=1)
-    assert 'accounts/login' in response.url
+def test_product_detail_unauthenticated(factory, product):
+
+  path = reverse('detail', kwargs={'pk': 1})
+  request = factory.get(path)
+  request.user = AnonymousUser()
+
+  response = product_detail(request, pk=1)
+  assert 'accounts/login' in response.url
+
